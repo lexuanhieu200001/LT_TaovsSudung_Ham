baf1fa0485044abcb69b27103c352a5ec881d7ce
LEXUANHIEU <lexuanhieu200001@gmail.com>
2023-11-22 10:02:36 +0700

    baitap

---
+++ BAITAP.js
@@ -0,0 +1,17 @@
+//bai1
+let n1 = +prompt('number')
+function binhphuong(n1){
+    return Math.pow(x=n1, y=2);
+}
+//let x2 = binhphuong(n1)
+alert('bình phương number =' + binhphuong(n1));
+// //bai2 a
+function chuvi(n1){
+    return 2*Math.PI*n1;
+}
+alert('chu vi hình tròn = ' + chuvi(n1));
+// bai 2b
+function dientich(n1) {
+    return n1*n1*Math.PI;
+}
+alert('diện tích hình tròn =' + dientich(n1));
